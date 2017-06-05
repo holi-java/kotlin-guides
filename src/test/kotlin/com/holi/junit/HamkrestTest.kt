b7e235c0b20490be521c171f077a74d4c9d8e89e
holi <32414154@qq.com>
2017-06-06 01:21:42 +0800

    testing hamkrest

---
+++ src/test/kotlin/com/holi/junit/HamkrestTest.kt
@@ -0,0 +1,29 @@
+package com.holi.junit
+
+import com.natpryce.hamkrest.and
+import com.natpryce.hamkrest.assertion.assert
+import com.natpryce.hamkrest.endsWith
+import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.or
+import org.junit.Test
+
+/**
+ * Created by holi on 6/6/17.
+ */
+class HamkrestTest {
+    @Test
+    fun simple() {
+        assert.that("foo", equalTo("foo"));
+    }
+
+    @Test
+    fun conjunction() {
+        assert.that("foo", equalTo("foo") and endsWith("oo"));
+    }
+
+    @Test
+    fun `dis-conjunction`() {
+        assert.that("foo", equalTo("foo") or equalTo("bar"));
+        assert.that("bar", equalTo("foo") or equalTo("bar"));
+    }
+}
