a8c24b940fca99b5bb80b6539a0562171b6ec836
holi <32414154@qq.com>
2017-06-06 00:00:37 +0800

    testing knit

---
+++ src/test/kotlin/com/holi/junit/KNitTest.kt
@@ -0,0 +1,25 @@
+package com.holi.junit
+
+import com.taroid.knit.should
+import org.junit.Assert.fail
+import org.junit.Test
+
+/**
+ * Created by holi on 6/5/17.
+ */
+class KNitTest {
+    @Test
+    fun equality() {
+        "foo".should be "foo";
+    }
+
+    @Test
+    fun `not be`() {
+        "foo".should notBe "bar";
+    }
+
+    @Test(expected = AssertionError::class)
+    fun `fails with expected exception`() {
+        fail("ok");
+    }
+}
