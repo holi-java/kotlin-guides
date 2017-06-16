55911cced7dc8d35d692bac05d62196e31a8fbbf
holi <32414154@qq.com>
2017-06-17 01:54:38 +0800

    short-circuiting operation of safe call operator

--- src/test/kotlin/com/holi/basic/operators/NullSafetyOperatorsTest.kt
+++ src/test/kotlin/com/holi/basic/operators/NullSafetyOperatorsTest.kt
@@ -1,11 +1,9 @@
 package com.holi.basic.operators
 
-import com.natpryce.hamkrest.absent
-import com.natpryce.hamkrest.assertion.assert;
-import com.natpryce.hamkrest.equalTo
-import com.natpryce.hamkrest.isA
-import com.natpryce.hamkrest.throws
+import com.natpryce.hamkrest.*
+import com.natpryce.hamkrest.assertion.assert
 import org.junit.Test
+import kotlin.jvm.internal.Ref.ObjectRef
 
 class NullSafetyOperatorsTest {
     val foo: String? = "bar";
@@ -23,5 +21,43 @@ class NullSafetyOperatorsTest {
     fun `unsafe call`() {
         assert.that(foo!!.length, equalTo(3));
         assert.that({ nil!!.length }, throws(isA<NullPointerException>()));
+    }
+
+    @Test
+    fun `short-circuiting`() {
+        val none: ObjectRef<Unit>? = null;
+        val value: ObjectRef<Unit>? = ObjectRef();
+        var calls = 0;
+        fun call() {
+            calls++;
+        }
+
+
+        none?.element = call();
+        assert.that(calls, equalTo(0));
+
+        value?.element = call();
+        assert.that(calls, equalTo(1));
+
+        value?.element = call();
+        assert.that(calls, equalTo(2));
+    }
+
+    @Test
+    fun `copy the reference on short-circuiting`() {
+        var it: ObjectRef<Int>? = ObjectRef();
+        val expected = it;
+        var received: Any? = null;
+
+        fun call(): Int {
+            received = it;
+            it = null;
+            return 1;
+        }
+
+        it?.element = call();
+
+        assert.that(received, sameInstance<Any?>(expected));
+        assert.that(it, absent());
     }
 }
