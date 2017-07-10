e88a9ad3c54453092519a74d3077da6b96ac9a94
holi <32414154@qq.com>
2017-07-11 05:00:35 +0800

    add spread operator tests

--- src/test/kotlin/com/holi/basic/operators/ArraySpreadOperatorTest.kt
+++ src/test/kotlin/com/holi/basic/operators/ArraySpreadOperatorTest.kt
@@ -3,6 +3,8 @@ package com.holi.basic.operators
 import com.natpryce.hamkrest.assertion.assert;
 import com.natpryce.hamkrest.equalTo
 import org.junit.Test
+import java.util.*
+import java.util.Arrays.asList
 
 class ArraySpreadOperatorTest {
 
@@ -13,5 +15,19 @@ class ArraySpreadOperatorTest {
         fun sum(vararg items: Int) = items.sum();
 
         assert.that(sum(*args), equalTo(9));
+    }
+
+    @Test
+    fun `spread arrays into another array`() {
+        val array = arrayOf(1, *arrayOf(2, 3), *arrayOf(*arrayOf(4)), 5)
+
+        assert.that(array.toList(), equalTo(listOf(1, 2, 3, 4, 5)))
+    }
+
+    @Test
+    fun `spread array contains null`() {
+        val strings = asList("foo", *arrayOf<String?>(null), "bar")
+
+        assert.that(strings, equalTo(listOf("foo", null, "bar")))
     }
 }
